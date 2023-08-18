00ea33549ba1553a43a5a172549d716a4388a249
Andrés Sánchez <113924667+AndresF-SanchezG@users.noreply.github.com>
2023-08-17 19:32:58 -0500

    Update index.js

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -10,7 +10,7 @@ function TodoSearch() {
     
 
     return(
-      <input placeholder='Cortar Cebolla' className="TodoSearch" value={searchValue} onChange={(event)=>{
+      <input placeholder='Presiona (+) e ingresa tu primer TODO' className="TodoSearch" value={searchValue} onChange={(event)=>{
         setSearchValue(event.target.value)
 
       }}/>
